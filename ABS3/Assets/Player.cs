c995d883d908a2ab5733d209b17a69623bc88338
TomoatHowtoTomo101 <51061088+TomoatHowtoTomo101@users.noreply.github.com>
2020-11-21 11:23:16 -0600

    Most Of The Stuff (Minus The Library)
    Library stuff can be a little tricky

---
+++ ABS3/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Health = 100;
+    public int Meter = 0;
+    public bool Max_Power = false;
+    public int BlastStock = 0;
+    public bool Assist = false;
+    public int AssistChar = 0;
+    public int ClothingDamage = 0;
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
